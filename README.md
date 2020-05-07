e5e1836e087bf4a9d7c233f5051af47158779b77
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-05-08 00:36:15 +0300

    Prepare heroku config

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # angular-todo-app
 My second homework of angular course.
 
+My app on heroku - https://guarded-caverns-22892.herokuapp.com/
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-//Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
  
-const app = express();
- 
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/angular-todo-app'));
- 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/angular-todo-app/index.html'));
-});
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/todo-app'));
+
 app.listen(process.env.PORT || 8080);
+ 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/todo-app/index.html'));
+});
+
+console.log('Console listening!');
